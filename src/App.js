4e1adede82c81d3a96c67c1c945f3f85586ae777
luismav1 <luismav2001@hotmail.com>
2024-05-27 22:26:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+import CompShowBlogs from './blog/ShowBlogs.js';
+import CompCreateBlog from './blog/CreateBlog.js';
+import CompEditBlog from './blog/EditBlog.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<CompShowBlogs/>}/>
+          <Route path='/create' element={<CompCreateBlog/>}/>
+          <Route path='/edit/:id' element={ <CompEditBlog />} />
+        </Routes>
+      </BrowserRouter>
+    
+    </div>
+  );
+}
+
+export default App;
